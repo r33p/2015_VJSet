8beccbabcbaf26ea1bfc71d332d98254d01f56b1
r33p <pierre@314r.net>
2015-09-02 12:22:06 +0200

    maj depuis backup juillet 2015

--- LIVE.sh
+++ LIVE.sh
@@ -1,3 +1,9 @@
-modprobe v4l2loopback &&
-ffmpeg -f x11grab -r 25 -s 256x256 -follow_mouse centered -show_region 1 -i :0.0+0,0 -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 &&
-puredata -rt -sleepgrain 1 -noaudio -alsamidi ./LIVE.pd 
+#!/bin/bash
+puredata -path /home/rep/Sources/Gem/Gem/ -open /home/rep/LIVE/2015_VJSet/LIVE.pd -alsamidi
+
+#modprobe v4l2loopback &&
+#ffmpeg -f x11grab -r 25 -s 256x256 -follow_mouse centered -show_region 1 -i :0.0+0,0 -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 &&
+#puredata -rt -sleepgrain 1 -noaudio -alsamidi ./LIVE.pd 
+
+
+
